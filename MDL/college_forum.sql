95d664bb980a6353dbefcbd5d541e3c40fe68fba
adil mostapha EL AMRANI <adil.elamrani215@gmail.com>
2023-11-20 14:21:09 -0500

    organisation de mon dossier

---
+++ MDL/college_forum.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE college_forum;
+
+CREATE TABLE Utilisateur (
+    id_Utilisateur INT AUTO_INCREMENT PRIMARY KEY,
+    nomUtilisateur VARCHAR(45) UNIQUE,
+    nom VARCHAR(45),
+    MotDePasse VARCHAR(20),
+    DateDeNaissance DATE
+);
+
+CREATE TABLE Forum (
+    id_Article INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(100),
+    article varchar(255),
+    datePublication DATE,
+    auteur varchar(45),
+    id_Utilisateur INT,
+    FOREIGN KEY (id_Utilisateur) REFERENCES Utilisateur(id_Utilisateur)
+);
